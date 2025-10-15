ff991a4d186146957c361e7de4e8c5f1a65c9c1e
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-10-15 17:16:26 +0300

    feat: импорт css стилей

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './styles/variables.css'
+import 'normalize.css'
+import './styles/globals.css'
+import App from './components/App/App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
