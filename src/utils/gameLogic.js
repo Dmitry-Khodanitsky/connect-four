74dc15103186062ea6b6fa498e3abc356d2299d4
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-10-16 14:33:11 +0300

    feat: добавить логику хода игрока

--- src/utils/gameLogic.js
+++ src/utils/gameLogic.js
@@ -1,11 +1,25 @@
-export const findLowestEmptyCell = (board, column) => {
-
+const findLowestEmptyCell = (board, column) => {
   for (let row = board.length - 1; row >= 0; row--) {
     if (board[row][column] === null) {
       return { row, column }
     }
   }
-  return null 
+  return null
 }
 
-export default findLowestEmptyCell
+const makeMove = (gameState, column) => {
+  const { board, currentPlayer } = gameState
+  const emptyCell = findLowestEmptyCell(board, column)
+  if (!emptyCell) return null
+
+  const newBoard = board.map((row) => [...row])
+  newBoard[emptyCell.row][column] = currentPlayer
+
+  return {
+    board: newBoard,
+    row: emptyCell.row,
+    col: column,
+  }
+}
+
+export { findLowestEmptyCell, makeMove }
