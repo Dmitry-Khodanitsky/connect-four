fcb0dbf8aacc923b429c392651a868459d84e310
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-10-17 11:32:17 +0300

    feat: добавить логику отображения статуса игры Отображение зависит от того закончена ли игра, есть ли победитель

--- src/components/status-panel/StatusPanel.jsx
+++ src/components/status-panel/StatusPanel.jsx
@@ -1,12 +1,46 @@
 import './StatusPanel.css'
 
-const StatusPanel = ({ currentPlayer, gameResult }) => {
-  const playerTurn = currentPlayer === 'X' ? 'Ход игрока 1' : 'Ход игрока 2'
-  const colorClass = currentPlayer === 'X' ? 'player_1' : 'player_2'
+const StatusPanel = ({ currentPlayer, gameWinner, gameStatus }) => {
+
+  const getStatusContent = () => {
+    switch (gameStatus) {
+      case 'pending':
+        const playerTurn =
+          currentPlayer === 'X' ? 'Ход игрока 1' : 'Ход игрока 2'
+        const turnColorClass = currentPlayer === 'X' ? 'player_1' : 'player_2'
+        return {
+          text: playerTurn,
+          className: turnColorClass,
+        }
+
+      case 'win':
+        const winnerText =
+          gameWinner === 'X' ? 'Победил игрок 1!' : 'Победил игрок 2!'
+        const winnerColorClass = gameWinner === 'X' ? 'player_1' : 'player_2'
+        return {
+          text: winnerText,
+          className: winnerColorClass,
+        }
+
+      case 'draw':
+        return {
+          text: 'Ничья!',
+          className: 'status_draw',
+        }
+
+      default:
+        return {
+          text: 'Начните игру',
+          className: 'status_default',
+        }
+    }
+  }
+
+  const { text, className } = getStatusContent()
 
   return (
-    <div className={`status_panel ${colorClass}`}>
-      <p>{playerTurn}</p>
+    <div className={`status_panel ${className}`}>
+      <p>{text}</p>
     </div>
   )
 }
