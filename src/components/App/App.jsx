20f6fb37ef39e099340c738e5063ace47c2eeaa6
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-10-20 19:09:49 +0300

    refactor: Использовать хук useGame, который был вынесен в отдельный модуль

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,72 +1,34 @@
-import { useState } from 'react'
 import './App.css'
-import Board from '../board' 
-import StatusPanel from '../status-panel' 
+import Board from '../board'
+import StatusPanel from '../status-panel'
 import ScoreBox from '../score-box'
 import GameFieldHeader from '../game-field-header'
-import { makeMove } from '../../utils/gameLogic'
+import { useGame } from '../../hooks/useGame'
+
 
 function App() {
-  const ROWS = 6
-  const COLS = 7
-  const PLAYER1 = 'X'
-  const PLAYER2 = '0'
-
-  const initialState = {
-    board: Array(ROWS)
-      .fill()
-      .map(() => Array(COLS).fill(null)),
-    currentPlayer: PLAYER1,
-    winner: null,
-    gameStatus: 'pending', //waiting - игра не началась, win, draw
-  }
-
-  const [gameState, setGameState] = useState(initialState)
-  const [gameStatus, setGameStatus] = useState('pending') // pending, waiting - игра не началась, win, draw
-  const [gameScore, setGameScore] = useState({
-    player_1: 10,
-    player_2: 5,
-  })
-
-  const handleMove = (colIndex) => {
-    setGameState((prevState) => {
-      const moveResult = makeMove(prevState, colIndex)
-      if (!moveResult) {
-        // Колонка заполнена, ход невозможен
-        return prevState
-      }
-
-      const newGameState = {
-        ...prevState,
-        board: moveResult.board,
-        currentPlayer: prevState.currentPlayer === PLAYER1 ? PLAYER2 : PLAYER1,
-        winner: null, // тут должна быть функция определения победителя
-      }
-
-      return newGameState
-    })
-  }
-
-  const handleRestart = () => {
-    setGameState(initialState)
-    setGameScore({
-      player_1: 0,
-      player_2: 0,
-    })
-  }
-
+  const {
+    gameState,
+    gameStatus,
+    gameScore,
+    handleMove,
+    handleRestart,
+    handlePlayAgain,
+  } = useGame()
+  
   return (
-    <main className="game_field">
+    <main className="game-field">
       <GameFieldHeader onRestart={handleRestart} />
       <section className="board-box">
-        <ScoreBox score={gameScore.player_1} player="player_1" />
-      <Board gameState={gameState} onClick={handleMove} />
-        <ScoreBox score={gameScore.player_2} player="player_2" />
+        <ScoreBox score={gameScore.player_1} player="player-1" />
+        <Board gameState={gameState} onClick={handleMove} />
+        <ScoreBox score={gameScore.player_2} player="player-2" />
       </section>
       <StatusPanel
         currentPlayer={gameState.currentPlayer}
         gameWinner={gameState.winner}
         gameStatus={gameStatus}
+        onPlayAgain={handlePlayAgain}
       />
     </main>
   )
