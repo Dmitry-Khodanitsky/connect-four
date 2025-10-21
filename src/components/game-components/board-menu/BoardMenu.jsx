4fff70f5cb6a4e3027f62589717df0833d859a89
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-10-21 20:37:35 +0300

    refactor: - Переименовать ккомпонент gameFieldHeader в BoardMenu
    - Переместить компонент BoardMenu в новую директорию
    
    -Создать новую структуру директорий компонентов, components -> game-components
    Переместить BoardMenu в эту директорию

---
+++ src/components/game-components/board-menu/BoardMenu.jsx
@@ -0,0 +1,23 @@
+import './BoardMenu.css'
+import Button from '../../ui/button'
+
+const GameFieldHeader = ({ onRestart }) => {
+  return (
+    <header className="board-menu">
+      <div className="board-menu-content">
+        <Button className="board-menu">
+          Правила
+        </Button>
+        <h1>4 в ряд </h1>
+        <Button
+          onClick={onRestart}
+          className="board-menu board-menu--restart"
+        >
+          Рестарт
+        </Button>
+      </div>
+    </header>
+  )
+}
+
+export default GameFieldHeader
