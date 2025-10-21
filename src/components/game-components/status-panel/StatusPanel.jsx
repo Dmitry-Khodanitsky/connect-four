611d3606ddb129e3e54fd393cb599b91ab13e4d6
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-10-21 20:35:43 +0300

    refactor: переместить компонент StatusPanel в новую директорию
    -Создать новую структуру директорий компонентов, components -> game-components
    Переместить StatusPanel в эту директорию

---
+++ src/components/game-components/status-panel/StatusPanel.jsx
@@ -0,0 +1,47 @@
+import './StatusPanel.css'
+import Button from '../../ui/button'
+
+const StatusPanel = ({ currentPlayer, gameWinner, gameStatus, onPlay }) => {
+  const getStatusContent = () => {
+    switch (gameStatus) {
+      case 'pending':
+        return {
+          text: currentPlayer.moveText,
+          className: currentPlayer.className,
+          startButton: false,
+        }
+
+      case 'win':
+        return {
+          text: gameWinner.winText,
+          className: gameWinner.className,
+          startButton: true,
+        }
+
+      case 'draw':
+        return {
+          text: 'Ничья!',
+          className: 'status-draw',
+          startButton: true,
+        }
+
+      default:
+        return {
+          text: 'Начать игру',
+          className: 'status-default',
+          startButton: true,
+        }
+    }
+  }
+
+  const { text, className, startButton } = getStatusContent()
+
+  return (
+    <div className={`status-panel ${className}`}>
+      <p>{text}</p>
+      {startButton && <Button className="status-panel" onClick={onPlay}>Сыграть еще</Button>}
+    </div>
+  )
+}
+
+export default StatusPanel
