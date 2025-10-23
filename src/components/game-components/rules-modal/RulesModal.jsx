67593295ed4e4cbea329f2fa3705781da7b7b795
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-10-23 15:00:11 +0300

    feat: добавить компонент модального окна с правилами игры

---
+++ src/components/game-components/rules-modal/RulesModal.jsx
@@ -0,0 +1,34 @@
+import './RulesModal.css'
+import Button from '../../ui/button'
+import GAME_RULES from '../../../constants'
+
+const Modal = ({ onClose }) => {
+  return (
+    <div className="modal">
+      <div className="modal-header">
+        <div className="modal-title">
+          <h2>{GAME_RULES.title}</h2>
+        </div>
+      </div>
+      <div className="modal-body">
+        <h3>Цель игры</h3>
+        <p>{GAME_RULES.description}</p>
+        <h3>Как играть?</h3>
+        <ol>
+          {GAME_RULES.rules.map((rule, index) => {
+            return (<li key={index}>{rule}</li>)
+          })}
+        </ol>
+      </div>
+      <div className="modal-footer">
+        <Button
+          type={'close-modal'}
+          aria-label="Закрыть модальное окно"
+          onClick={onClose}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Modal
