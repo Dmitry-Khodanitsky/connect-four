c8ca773811e6e67dfc411fbe7290df907d2255d0
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-10-23 15:39:14 +0300

    feat: добавлен оверлей, который открывается при окрытии модального окна на заднем плане

--- src/components/game-components/rules-modal/RulesModal.jsx
+++ src/components/game-components/rules-modal/RulesModal.jsx
@@ -4,28 +4,30 @@ import GAME_RULES from '../../../constants'
 
 const Modal = ({ onClose }) => {
   return (
-    <div className="modal">
-      <div className="modal-header">
-        <div className="modal-title">
-          <h2>{GAME_RULES.title}</h2>
+    <div className="modal-overlay">
+      <div className="modal">
+        <div className="modal-header">
+          <div className="modal-title">
+            <h2>{GAME_RULES.title}</h2>
+          </div>
         </div>
-      </div>
-      <div className="modal-body">
-        <h3>Цель игры</h3>
-        <p>{GAME_RULES.description}</p>
-        <h3>Как играть?</h3>
-        <ol>
-          {GAME_RULES.rules.map((rule, index) => {
-            return (<li key={index}>{rule}</li>)
-          })}
-        </ol>
-      </div>
-      <div className="modal-footer">
-        <Button
-          type={'close-modal'}
-          aria-label="Закрыть модальное окно"
-          onClick={onClose}
-        />
+        <div className="modal-body">
+          <h3>Цель игры</h3>
+          <p>{GAME_RULES.description}</p>
+          <h3>Как играть?</h3>
+          <ol>
+            {GAME_RULES.rules.map((rule, index) => {
+              return <li key={index}>{rule}</li>
+            })}
+          </ol>
+        </div>
+        <div className="modal-footer">
+          <Button
+            type={'close-modal'}
+            aria-label="Закрыть модальное окно"
+            onClick={onClose}
+          />
+        </div>
       </div>
     </div>
   )
