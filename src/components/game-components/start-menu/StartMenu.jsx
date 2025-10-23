94a12e0d4fdb5530ac5df90bccd6de7af26250a0
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-10-23 15:07:09 +0300

    feat: добавить модальное окно с правилами игры
    - Добавить состояние isModalVisible для управления видимостью модалки
    - Реализовать обработчики открытия/закрытия модального окна
    - Интегрировать компонент Modal в StartMenu
    - Обновить кнопку 'Правила' для открытия модалки"

--- src/components/game-components/start-menu/StartMenu.jsx
+++ src/components/game-components/start-menu/StartMenu.jsx
@@ -1,14 +1,29 @@
+import { useState } from 'react'
 import './StartMenu.css'
 import Button from '../../ui/button'
+import Modal from '../rules-modal'
+
 const StartMenu = ({ onPlay }) => {
+  const [isModalVisible, setIsModalVisible] = useState(false)
+
+  const handleOpenModal = () => {
+    setIsModalVisible(true)
+  }
+
+  const handleCloseModal = () => {
+    setIsModalVisible(false)
+  }
+
   return (
     <div className="start-menu">
       <h1>4 в ряд </h1>
-      <Button className={'menu'} onClick={onPlay}>
+      <Button type={'menu'} onClick={onPlay}>
         Начать игру
       </Button>
-      <Button className={'menu'}>Выбрать персонажа</Button>
-      <Button className={'menu'}>Правила</Button>
+      <Button type={'menu'} onClick={handleOpenModal}>
+        Правила
+      </Button>
+      {isModalVisible && <Modal onClose={handleCloseModal}> Rules </Modal>}
     </div>
   )
 }
