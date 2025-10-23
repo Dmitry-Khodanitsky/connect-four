25cdeb56da16cb9fff51c5355c03c02aa66c9110
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-10-23 15:53:52 +0300

    feat: добавить модальное окно при нажатии на кнопку "Правила" на игровом поле

--- src/components/game-components/board-menu/BoardMenu.jsx
+++ src/components/game-components/board-menu/BoardMenu.jsx
@@ -1,15 +1,27 @@
+import { useState } from 'react'
 import './BoardMenu.css'
 import Button from '../../ui/button'
+import Modal from '../rules-modal'
 
 const GameFieldHeader = ({ onRestart }) => {
+  const [isModalVisible, setIsModalVisible] = useState(false)
+
+  const handleOpenModal = () => {
+    setIsModalVisible(true)
+  }
+
+  const handleCloseModal = () => {
+    setIsModalVisible(false)
+  }
   return (
     <header className="board-menu">
       <div className="board-menu-content">
-        <Button type="board-menu">Правила</Button>
+        <Button type="board-menu" onClick={handleOpenModal}>Правила</Button>
         <h1>4 в ряд </h1>
         <Button onClick={onRestart} type="board-menu board-menu--restart">
           Рестарт
         </Button>
+         {isModalVisible && <Modal onClose={handleCloseModal}> Rules </Modal>}
       </div>
     </header>
   )
