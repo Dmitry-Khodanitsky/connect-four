ca7cf3d91b4fc7fddbf5433f093b729495c7edbd
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-10-24 14:27:48 +0300

    refactor: добавить пропс score
    -Передано состояние score  в пропсе.
    
    Компонент отображает текущий счет игры из пропса score

--- src/components/game-components/score-box/ScoreBox.jsx
+++ src/components/game-components/score-box/ScoreBox.jsx
@@ -1,12 +1,13 @@
 import './ScoreBox.css'
 
-const ScoreBox = ({ player }) => {
-  const { className, name, score } = player
+const ScoreBox = ({ player, score }) => {
+  const { className, name } = player
+
   return (
     <div className={`score-box score-box--${className}`}>
       <div className="player-avatar"></div>
       <div className="score-info">
-        {name} Счет: {score}
+        {name} Счет: {score[player.id]}
       </div>
     </div>
   )
