f15c1d7f634faac283a320cce68fda0a65ce8240
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-10-25 12:41:03 +0300

    refactor: изменить дефолтное условие  для swich - Изменено дефолтное условие в swtich, теперь по дефорту состояние 'pending'

--- src/components/status-panel/statusPanelHelper.js
+++ src/components/status-panel/statusPanelHelper.js
@@ -1,37 +1,28 @@
- const getStatusContent = (gameStatus, currentPlayer, gameWinner) => {
-    switch (gameStatus) {
-      case 'pending':
-        return {
-          text: currentPlayer.moveText,
-          className: currentPlayer.className,
-          startButton: false,
-          showWinAnimation: false,
-        }
+const getStatusContent = (gameStatus, currentPlayer, gameWinner) => {
+  switch (gameStatus) {
+    case 'win':
+      return {
+        text: gameWinner.winText,
+        className: gameWinner.className,
+        startButton: true,
+        showWinAnimation: true,
+      }
 
-      case 'win':
-        return {
-          text: gameWinner.winText,
-          className: gameWinner.className,
-          startButton: true,
-          showWinAnimation: true,
-        }
-
-      case 'draw':
-        return {
-          text: 'Ничья!',
-          className: 'status-draw',
-          startButton: true,
-          showWinAnimation: false,
-        }
-
-      default:
-        return {
-          text: 'Начать игру',
-          className: 'status-default',
-          startButton: true,
-          showWinAnimation: false,
-        }
-    }
+    case 'draw':
+      return {
+        text: 'Ничья!',
+        className: 'status-draw',
+        startButton: true,
+        showWinAnimation: false,
+      }
+    default:
+      return {
+        text: currentPlayer.moveText,
+        className: currentPlayer.className,
+        startButton: false,
+        showWinAnimation: false,
+      }
   }
+}
 
-  export default getStatusContent
+export default getStatusContent
