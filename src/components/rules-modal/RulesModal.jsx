ca1ba7c9811e6ac4d602195f1f1682f38f133f75
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-10-25 13:36:55 +0300

    feat: использовать ьбиблиотеку react-modal для обертки моадльного окна с правилами

--- src/components/rules-modal/RulesModal.jsx
+++ src/components/rules-modal/RulesModal.jsx
@@ -1,36 +1,43 @@
 import './RulesModal.css'
+import '../../shared/styles/modal.css'
 import Button from '../../shared/ui/button'
 import GAME_RULES from '../../shared/constants'
+import Modal from 'react-modal'
 
-const Modal = ({ onClose }) => {
+
+const RulesModal = ({ onClose, isModalVisible }) => {
   return (
-    <div className="modal-overlay">
-      <div className="modal">
-        <div className="modal-header">
-          <div className="modal-title">
-            <h2>{GAME_RULES.title}</h2>
-          </div>
-        </div>
-        <div className="modal-body">
-          <h3>Цель игры</h3>
-          <p>{GAME_RULES.description}</p>
-          <h3>Как играть?</h3>
-          <ol>
-            {GAME_RULES.rules.map((rule, index) => {
-              return <li key={index}>{rule}</li>
-            })}
-          </ol>
-        </div>
-        <div className="modal-footer">
-          <Button
-            type={'close-modal'}
-            aria-label="Закрыть модальное окно"
-            onClick={onClose}
-          />
+    <Modal
+      isOpen={isModalVisible}
+      onRequestClose={onClose}
+      className="modal"
+      overlayClassName="modal-overlay"
+      contentLabel="Rules Modal"
+    >
+      <div className="modal-header">
+        <div className="modal-title">
+          <h2>{GAME_RULES.title}</h2>
         </div>
       </div>
-    </div>
+      <div className="modal-body">
+        <h3>Цель игры</h3>
+        <p>{GAME_RULES.description}</p>
+        <h3>Как играть?</h3>
+        <ol>
+          {GAME_RULES.rules.map((rule, index) => {
+            return <li key={index}>{rule}</li>
+          })}
+        </ol>
+      </div>
+      <div className="modal-footer">
+        <Button
+          type={'close-modal'}
+          aria-label="Закрыть модальное окно"
+          onClick={onClose}
+        />
+      </div>
+    </Modal>
   )
 }
 
-export default Modal
+export default RulesModal
