26a8c0696ed76bd9b94c2e53bcdf3b1c2b1d5e6b
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-10-27 15:07:50 +0300

    fix: указать основной элемент для react-modal чтобы он делался недоступным для скринридеров при открытом модальном окне

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { StartMenu, GameScreen } from '../screens'
 import { useGame } from '../shared/hooks'
+import Modal from 'react-modal'
 
 function App() {
   const {
@@ -13,22 +14,24 @@ function App() {
     handlePlay,
   } = useGame()
 
+  // setAppElement это функция из библиотеки react-modal, которая позволяет указать, какой HTML-элемент является основным контейнером вашего приложения. Когда модальное окно открывается, react-modal использует эту информацию, чтобы сделать основной контент страницы недоступным для скринридеров (программ для чтения с экрана). Это достигается путем добавления атрибута aria-hidden="true" к указанному элементу.
+
+  Modal.setAppElement('#root')
+
   if (gameStatus === 'waiting') {
-    return (
-        <StartMenu onPlay={handlePlay} />
-    )
+    return <StartMenu onPlay={handlePlay} />
   }
 
   return (
-      <GameScreen
-        gameState={gameState}
-        gameStatus={gameStatus}
-        gamePlayers={gamePlayers}
-        score={score}
-        handleMove={handleMove}
-        handleRestart={handleRestart}
-        handlePlay={handlePlay}
-      />
+    <GameScreen
+      gameState={gameState}
+      gameStatus={gameStatus}
+      gamePlayers={gamePlayers}
+      score={score}
+      handleMove={handleMove}
+      handleRestart={handleRestart}
+      handlePlay={handlePlay}
+    />
   )
 }
 
