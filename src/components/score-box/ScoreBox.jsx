965cb7c689eeef163dd1b93dcfd9b3864a0944a8
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-10-27 15:47:35 +0300

    feat: добавить анимацию появления табличек со счетом -Использована библиотека frame-motion для анимации

--- src/components/score-box/ScoreBox.jsx
+++ src/components/score-box/ScoreBox.jsx
@@ -1,15 +1,20 @@
 import './ScoreBox.css'
+import { motion } from 'framer-motion'
+import { slideDown } from '@shared/animations/constants'
 
 const ScoreBox = ({ player, score }) => {
   const { className, name } = player
 
   return (
-    <div className={`score-box`}>
+    <motion.div
+      className={`score-box`}
+      {...slideDown}
+    >
       <div className={`player-avatar ${className}`}></div>
       <div className="score-info">
         {name} Счет: {score[player.id]}
       </div>
-    </div>
+    </motion.div>
   )
 }
 
