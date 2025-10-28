8abf74ccd439305dc1287bdfd31f9a056b576068
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-10-28 09:51:09 +0300

    chore: настроить линтер для работы с react
    -Остановлена проверка на типы пропсов
    -Добавлен плагин react
    -Добавлены рекомендованные плагины и конфиги

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
@@ -8,10 +9,15 @@ export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
+    plugins: {
+      react,
+    },
     extends: [
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      react.configs.flat.recommended,
+      react.configs.flat['jsx-runtime'],
     ],
     languageOptions: {
       ecmaVersion: 2020,
@@ -24,6 +30,12 @@ export default defineConfig([
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react/prop-types': 'off', // убрать в версии TypeScript
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
 ])
