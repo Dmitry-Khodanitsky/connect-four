0b1e969be9249ab4ba7cd337a1bddd7df2692ade
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-10-29 16:34:31 +0300

    feat: добавить типы для хука useGame

---
+++ src/shared/hooks/useGame.types.ts
@@ -0,0 +1,13 @@
+import { ACTION_TYPES } from '../constants/gameConstants'
+import type { CombinedGameState } from '../constants/gameTypes'
+
+export interface UseGameReturn extends CombinedGameState {
+  handleMove: (columnIndex: number) => void
+  handleRestart: () => void
+  handlePlay: () => void
+}
+
+export type Action =
+  | { type: typeof ACTION_TYPES.START_GAME }
+  | { type: typeof ACTION_TYPES.RESTART_GAME }
+  | { type: typeof ACTION_TYPES.MAKE_MOVE; payload: number }
