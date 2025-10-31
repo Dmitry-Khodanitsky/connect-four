00465f4c297e81d90c6012da40713e37f802a265
Dima Hodanitskiy <dimahodanitskiy@gmail.com>
2025-10-31 14:53:50 +0300

    feat: добавить типы для функций cellHelpers

--- src/components/board/cellHelpers.ts
+++ src/components/board/cellHelpers.ts
@@ -1,17 +1,23 @@
-// cellHelpers.js
-export const isWinningCell = (winCellsCombination, rowIndex, colIndex) => {
+import type { CellPosition } from '@shared/constants/gameConstants.types'
+import type { CellProps } from './Cell'
+
+export const isWinningCell = (
+  winCellsCombination: CellPosition[],
+  rowIndex: number,
+  colIndex: number,
+): boolean => {
   return winCellsCombination.some(
     (cell) => cell.row === rowIndex && cell.col === colIndex
   )
 }
 
 export const getClassNames = (
-  cell,
-  playersClasses,
-  winningCells,
-  rowIndex,
-  colIndex
-) => {
+  cell: CellProps['cell'],
+  playersClasses: CellProps['playersClasses'],
+  winningCells: CellProps['winningCells'],
+  rowIndex: CellProps['rowIndex'],
+  colIndex: CellProps['colIndex']
+): string => {
   const defaultClass = 'chip'
   const playerClass =
     cell === 'X' ? playersClasses.player1 : playersClasses.player2
